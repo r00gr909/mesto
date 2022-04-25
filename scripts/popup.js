1fcc14978717eddec128ae589ca6f3e7fde1d74e
Samyan Mongush <r00gr909@yandex.ru>
2022-04-25 17:28:57 +0700

    initial commit

---
+++ scripts/popup.js
@@ -0,0 +1,7 @@
+const popupOpenBtn = document.querySelector('.button__add');
+const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.button__close');
+
+popupOpenBtn.addEventListener('click', function() {
+  alert('xyi')
+})
