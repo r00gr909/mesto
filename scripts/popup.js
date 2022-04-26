52637673d4e2f68efe0e63d830fff035ced014e7
Samyan Mongush <r00gr909@yandex.ru>
2022-04-26 13:35:50 +0700

    new ficha

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,6 +3,7 @@ const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.button__close');
 const popupSave = document.querySelector('.button__saving');
 const AddBtn = document.querySelector('.button__add');
+let Avater = document.querySelector('.profile__avatar')
 let popupContainer = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
@@ -44,15 +45,10 @@ function formSubmitHandler (evt) {
   } else if (profileName.textContent === 'Буян') {
     alert('Невиный задрот XD');
     profileInfo.textContent = 'Путь до рекурта';
-  } else if (profileName.textContent === 'Буян') {
-    alert('Невиный задрот XD');
-    profileInfo.textContent = 'Путь до рекурта';
   } else if (profileName.textContent === 'Кристина') {
     alert('Любюлю тебя &#4326;');
     profileInfo.textContent = 'Самая красивая ${&#4326;}';
-  } else if (profileName.textContent === 'Дан-Хаяа') {
-    alert('Малыыыш, скучаю :(');
-    profileInfo.textContent = 'Самая милая малышка <3';
+    Avater.setAttribute('src','images/kristina.jpg')
   } else if (profileName.textContent === 'Амыр-Байлак') {
     alert('Амыр-Байлак Сенди-Хуурак - язык можно сломать');
     profileInfo.textContent = 'Senor web-разработчик';
