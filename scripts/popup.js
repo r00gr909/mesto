97162d81c55423fcb5d44e31f2e05920d690c4a7
Samyan Mongush <r00gr909@yandex.ru>
2022-04-26 18:36:17 +0700

    fixed popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,7 +3,6 @@ const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.button__close');
 const popupSave = document.querySelector('.button__saving');
 const AddBtn = document.querySelector('.button__add');
-let Avater = document.querySelector('.profile__avatar')
 let popupContainer = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_name');
 let jobInput = document.querySelector('.popup__input_job');
@@ -30,29 +29,6 @@ function formSubmitHandler (evt) {
   jobInput.getAttribute('value');
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
-  if (profileName.textContent === 'Муха') {
-    alert('Муха хуй сосет! XD');
-    profileInfo.textContent = 'Даю в попу';
-  } else if (profileName.textContent === 'Айдыс') {
-    alert('300? Отсоси у Тракториста, Айдис! XD');
-    profileInfo.textContent = 'Все глубже и глубже';
-  } else if (profileName.textContent === 'Дамир') {
-    alert('Стёепа - лизатель пихды! XD');
-    profileInfo.textContent = '500 руб/час';
-  } else if (profileName.textContent === 'Самян') {
-    alert('Властелин html  XD');
-    profileInfo.textContent = 'Шуточки подъехали';
-  } else if (profileName.textContent === 'Буян') {
-    alert('Невиный задрот XD');
-    profileInfo.textContent = 'Путь до рекурта';
-  } else if (profileName.textContent === 'Кристина') {
-    alert('Любюлю тебя &#4326;');
-    profileInfo.textContent = 'Самая красивая ${&#4326;}';
-    Avater.setAttribute('src','images/kristina.jpg')
-  } else if (profileName.textContent === 'Амыр-Байлак') {
-    alert('Амыр-Байлак Сенди-Хуурак - язык можно сломать');
-    profileInfo.textContent = 'Senor web-разработчик';
-  };
   RemovePopup();
 };
 
