0a4f4c508490cafd81c5cd78d466c07aa39e88fe
Samyan Mongush <r00gr909@yandex.ru>
2022-04-26 07:10:30 +0700

    initial commit

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,38 @@
-const popupOpenBtn = document.querySelector('.button__add');
+const popupEditBtn = document.querySelector('.button__edit');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.button__close');
+const popupSave = document.querySelector('.button__saving')
+let popupContainer = document.querySelector('.popup__container')
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let profileName = document.querySelector('.profile__name');
+let profileInfo = document.querySelector('.profile__specialization');
 
-popupOpenBtn.addEventListener('click', function() {
-  alert('300? отсоси у тракториста, Айдис')
-})
+function AddPopup () {
+  popup.classList.add('popup_active');
+  popupContainer.classList.add('popup__container_active');
+}
+
+function RemovePopup () {
+  popup.classList.remove('popup_active');
+  popupContainer.classList.remove('popup__container_active');
+}
+
+popupEditBtn.addEventListener('click', function() {
+  AddPopup();
+});
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  nameInput.getAttribute('value');
+  jobInput.getAttribute('value');
+  profileName.textContent = nameInput.value;
+  profileInfo.textContent = jobInput.value;
+  RemovePopup();
+};
+
+popupSave.addEventListener('click', formSubmitHandler);
+
+popupCloseBtn.addEventListener('click', function() {
+  RemovePopup();
+});
