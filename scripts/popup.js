cbbc6bf2731702328a22e12afaa8b5415e2c0e06
Samyan Mongush <r00gr909@yandex.ru>
2022-04-27 15:04:33 +0700

    fixed BEM

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,45 +1,41 @@
-const popupEditBtn = document.querySelector('.button__edit');
+const popupEditBtn = document.querySelector('.profile__btn_edit');
 const popup = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.button__close');
-const popupSave = document.querySelector('.button__saving');
-const AddBtn = document.querySelector('.button__add');
-let popupContainer = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+const popupCloseBtn = document.querySelector('.popup-container__btn_close');
+const popupSaveBtn = document.querySelector('.popup-container__btn_saving');
+let popupContainer = document.querySelector('.popup-container');
+let nameInput = document.querySelector('.popup-container__input_name');
+let jobInput = document.querySelector('.popup-container__input_job');
 let profileName = document.querySelector('.profile__name');
 let profileInfo = document.querySelector('.profile__specialization');
 
 function AddPopup () {
   popup.classList.add('popup_active');
-  popupContainer.classList.add('popup__container_active');
+  popupContainer.classList.add('popup-container_active');
 }
 
 function RemovePopup () {
   popup.classList.remove('popup_active');
-  popupContainer.classList.remove('popup__container_active');
+  popupContainer.classList.remove('popup-container_active');
 }
 
 popupEditBtn.addEventListener('click', function() {
   AddPopup();
 });
 
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameInput.getAttribute('value');
-  jobInput.getAttribute('value');
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
+  nameInput.setAttribute('value', profileName.textContent);
+  //let b = jobInput.getAttribute('value');
   RemovePopup();
 };
 
-popupSave.addEventListener('click', formSubmitHandler);
+popupSaveBtn.addEventListener('click',formSubmitHandler);
 
 popupCloseBtn.addEventListener('click', function() {
   RemovePopup();
 });
-
-AddBtn.addEventListener('click', function() {
-  alert('Fixed')
-})
 
 
