36e9025f95731df9b24eb2b32cf0f2b7804bb68c
Samyan Mongush <r00gr909@yandex.ru>
2022-05-02 09:38:28 +0700

    fixed popup

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@ const popupEditBtn = document.querySelector('.profile__btn_edit');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup-container__btn_close');
 const popupSaveBtn = document.querySelector('.popup-container__btn_saving');
-let popupContainer = document.querySelector('.popup-container');
 let nameInput = document.querySelector('.popup-container__input_name');
 let jobInput = document.querySelector('.popup-container__input_job');
 let profileName = document.querySelector('.profile__name');
@@ -10,32 +9,24 @@ let profileInfo = document.querySelector('.profile__specialization');
 
 function AddPopup () {
   popup.classList.add('popup_active');
-  popupContainer.classList.add('popup-container_active');
 }
 
 function RemovePopup () {
   popup.classList.remove('popup_active');
-  popupContainer.classList.remove('popup-container_active');
 }
 
-popupEditBtn.addEventListener('click', function() {
-  AddPopup();
-});
+popupEditBtn.addEventListener('click', AddPopup);
 
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileInfo.textContent = jobInput.value;
-  nameInput.setAttribute('value', profileName.textContent);
-  //let b = jobInput.getAttribute('value');
   RemovePopup();
 };
 
 popupSaveBtn.addEventListener('click',formSubmitHandler);
 
-popupCloseBtn.addEventListener('click', function() {
-  RemovePopup();
-});
+popupCloseBtn.addEventListener('click', RemovePopup);
 
 
