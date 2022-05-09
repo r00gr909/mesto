a0a1cfda8103c9614331143c71de53ff34ab4e39
Samyan Mongush <r00gr909@yandex.ru>
2022-05-09 15:35:32 +0700

    fixed popup.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,24 @@
-const popupEditBtn = document.querySelector('.profile__btn_edit');
-const popup = document.querySelector('.popup');
-const popupCloseBtn = document.querySelector('.popup-container__btn_close');
-const popupSaveBtn = document.querySelector('.popup-container__btn_saving');
 let nameInput = document.querySelector('.popup-container__input_name');
 let jobInput = document.querySelector('.popup-container__input_job');
 let profileName = document.querySelector('.profile__name');
 let profileInfo = document.querySelector('.profile__specialization');
 
+const popupEditBtn = document.querySelector('.profile__btn_edit');
+const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup-container__btn_close');
+const popupSaveBtn = document.querySelector('.popup-container__btn_saving');
+
+
+
 function AddPopup () {
   popup.classList.add('popup_active');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileInfo.textContent;
 }
 
 function RemovePopup () {
   popup.classList.remove('popup_active');
 }
-
-popupEditBtn.addEventListener('click', AddPopup);
-
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -25,8 +27,10 @@ function formSubmitHandler (evt) {
   RemovePopup();
 };
 
+popupEditBtn.addEventListener('click', AddPopup);
+popupCloseBtn.addEventListener('click', RemovePopup);
 popupSaveBtn.addEventListener('click',formSubmitHandler);
 
-popupCloseBtn.addEventListener('click', RemovePopup);
+
 
 
